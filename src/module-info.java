981b67d870615aa18a82cd02476207ae532e892d
kevinVtt <kevinvittor579@gmail.com>
2025-05-12 21:11:53 -0300

    modify module-info

--- src/module-info.java
+++ src/module-info.java
@@ -1,5 +1,9 @@
 module com.talentotech.preentrega {
-    exports com.talentotech.preentrega;      // Exporta com.talentotech.preentrega.Main
+    exports com.talentotech.preentrega;
     exports com.talentotech.preentrega.model;
-    exports com.talentotech.preentrega.service; // Asegura exportar los servicios
+    exports com.talentotech.preentrega.dao;
+    exports com.talentotech.preentrega.service;
+    exports com.talentotech.preentrega.excepciones;
+    exports com.talentotech.preentrega.interfaces;
+    exports com.talentotech.preentrega.service.productos;
 }
